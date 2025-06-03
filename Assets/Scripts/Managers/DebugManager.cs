bf2487901411ecae5b117dfdcde95d3b48f22b93
Grouuu <orewakamidayo@hotmail.com>
2025-06-04 01:08:34 +0200

    Delete local save when pressing ² on FR keyboard (` on US) - debug

---
+++ Assets/Scripts/Managers/DebugManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DebugManager : MonoBehaviour
+{
+	[SerializeField] private InputActionAsset _inputAction;
+
+	private InputAction _deleteGameLocalData;
+
+	private void OnEnable ()
+	{
+		_deleteGameLocalData = _inputAction.FindAction("Debug/DeleteGameLocalData");
+		_deleteGameLocalData.performed += DeleteGameLocalData;
+	}
+
+	private void OnDisable ()
+	{
+		_deleteGameLocalData.performed -= DeleteGameLocalData;
+	}
+
+	private void DeleteGameLocalData (InputAction.CallbackContext context)
+	{
+		if (Application.isFocused)
+		{
+			Debug.LogWarning("Destroyed local data");
+			PersistentManager.Instance.saveManager.DeleteSave();
+		}
+	}
+
+}
